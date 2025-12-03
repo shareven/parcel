8446c5999073ea6a05de27662f4113d4fb09dea1
shareven <1060776877@qq.com>
2025-12-03 17:34:49 +0800

    change version

--- 1build.sh
+++ 1build.sh
@@ -1,3 +1,3 @@
 ./gradlew assembleRelease
 
-cp app/build/outputs/apk/release/app-release.apk ~/Desktop/parcel-v1.0.37.apk
+cp app/build/outputs/apk/release/app-release.apk ~/Desktop/parcel-v1.0.38.apk
