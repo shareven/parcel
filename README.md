dc836e0593617e9e07d3ffa62695027a8012fd03
shareven <1060776877@qq.com>
2025-12-03 16:11:15 +0800

    安装包微小调整

--- README.md
+++ README.md
@@ -18,7 +18,7 @@
 
 # 下载 | Download  
 
-最新版本：v1.0.35
+最新版本：v1.0.36
 
 [Android apk release](https://github.com/shareven/parcel/releases/)
 
--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,8 +21,8 @@ android {
         applicationId = "com.xxxx.parcel"
         minSdk = 29
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.0.35"
+        versionCode = 36
+        versionName = "1.0.36"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
